0d5f751d4050b9f14000ee946d5055f374b91e5d
Dru Serkes <dru@zephyrx.com>
2021-04-02 10:00:17 -0400

    add translate steps

--- page.py
+++ page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 from locators import GoogleTranslateLocators
 
 
--- translate.py
+++ translate.py
@@ -4,7 +4,22 @@ from page import GoogleTranslate
 
 GOOGLE_TRANSLATE_URL = "https://translate.google.com/"
 
+example_phrase = "youngblood, say you want me back in your life"
+example_language = 'spanish'
+
 driver = webdriver.Chrome()
 driver.get(GOOGLE_TRANSLATE_URL)
 
 translator = GoogleTranslate(driver)
+
+translator.select_language(example_language)
+translator.type_phrase(example_phrase)
+translator.copy_translation()
+# TODO write to the file
+
+
+translator.clear_phrase()
+
+
+# when finished
+driver.quit()
